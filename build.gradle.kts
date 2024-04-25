b1ad3e4f6e8e042cbb304ca7f2856225718a8801
FlameyosFlow <89988527+FlameyosSnowy@users.noreply.github.com>
2024-04-25 12:01:10 +0200

    Add the OG gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("java")
+    id("maven-publish")
+    id("com.github.johnrengelman.shadow") version("8.1.1")
 }
 
 group = "me.flame.scheduler"
-version = "1.0-SNAPSHOT"
+version = "1.0.2"
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(11))
 
@@ -12,10 +14,30 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.jetbrains:annotations:24.0.1")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.shadowJar {
+    archiveClassifier.set("")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "mainRepository"
+            url = uri("https://repo.foxikle.dev/flameyos")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            artifact(tasks["shadowJar"])
+        }
+    }
 }
